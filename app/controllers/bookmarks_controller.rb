06bf87fb0a0f99cbf32f2d84cca2e58d2d9a90e6
lienxiong <lizkca@163.com>
2017-01-13 15:16:34 +0000

    del four line of application.html.haml

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,4 +7,41 @@ class BookmarksController < ApplicationController
     id = params[:id]
     @bookmark = Bookmark.find(id)
   end
+  
+  def new
+    @bookmark = Bookmark.new
+  end
+  
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      flash[:notice] = "#{@bookmark.address} was successfully created."
+      redirect_to bookmarks_path
+    end
+  end
+  
+  def edit 
+    @bookmark = Bookmark.find params[:id]
+  end
+  
+  def update
+    @bookmark = Bookmark.find params[:id]
+    @bookmark.update_attributes!(params[:bookmark])
+    flash[:notice] = "#{@bookmark.address} was successfully updated."
+    redirect_to bookmark_path(@bookmark)
+  end
+  
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    flash[:notice] = "Bookmark '#{@bookmark.address}' deleted."
+    redirect_to bookmarks_path
+  end
+  
+  private
+  
+  def bookmark_params
+    params.require(:bookmark).permit(:address, :description)
+  end
+    
 end
