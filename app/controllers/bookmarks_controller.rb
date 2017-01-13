387917b2af01165b982dee79cb25c354d74e5667
lienxiong <lizkca@163.com>
2017-01-13 10:53:03 +0000

    add show.html.haml

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,4 +2,9 @@ class BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
   end
+  
+  def show
+    id = params[:id]
+    @bookmark = Bookmark.find(id)
+  end
 end
