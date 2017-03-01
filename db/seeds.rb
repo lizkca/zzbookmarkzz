a32d1e7ed7ba736da0e6f1a31ca0805f9e1fea7c
lienxiong <lizkca@163.com>
2017-03-01 14:43:48 +0000

    use oga gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Bookmark.delete_all
 
-@doc = Nokogiri::HTML(File.open("db.html"))
+# @doc = Nokogiri::HTML(File.open("db.html"))
+# more_bookmarks = []
+# count = 0
+# @doc.xpath('//a').each do |bm|
+#   count = count + 1
+#   more_bookmarks << {:address => bm['href'],
+#                     :description => bm.text}
+#   end
+#   print count
+
+
+
+handle = File.open("db.html")
+@doc = Oga.parse_html(handle)
 more_bookmarks = []
-count = 0
-@doc.xpath('//a').each do |bm|
-  count = count + 1
-  more_bookmarks << {:address => bm['href'],
+
+d = @doc.xpath('//a')
+count_num = d.count
+
+print count_num
+d.each do |bm|
+  more_bookmarks << {:address => bm["HREF"],
                      :description => bm.text}
-  end
-  print count
+end
 
 more_bookmarks.each do |bookmark|
   Bookmark.create!(bookmark)
